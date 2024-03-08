18ecc0c0a5da3099702acc50c28087de4d362f57
hatd <ennino1234@gmail.com>
2024-03-08 15:20:29 +0700

    update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import HatdGetEth from './index.js';
 
-const hatdGetEth = new HatdGetEth()
-hatdGetEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975",).then(balance => { console.log("Balance without lib: ", balance) })
-hatdGetEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975", "GetEthBalance").then(balance => { console.log("Balance with lib: ", balance) })
+const getEth = new HatdGetEth()
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975",).then(balance => { console.log("Balance without lib: ", balance) })
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975", "GetEthBalance").then(balance => { console.log("Balance with lib: ", balance) })
